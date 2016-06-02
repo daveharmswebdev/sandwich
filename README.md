1a38edc8f8420e36a8b31b68503a6825ed12ebaa
Dave Harms <daveharmswebdev@gmail.com>
2016-06-02 11:30:43 -0500

    added proper readme file

---
+++ README.md
@@ -0,0 +1,29 @@
+# Sandwich Maker
+
+Nashville Software School student project. 
+
+### Installing
+
+Install with git clone.
+
+```bash
+git clone git@github.com:daveharmswebdev/sandwich.git
+```
+
+## Built With
+
+* HTML
+* CSS
+* JS
+
+## Authors
+
+**Dave Harms**
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Steve Brownlee, NSS Instructor
--- javascripts/domHandler.js
+++ javascripts/domHandler.js
@@ -4,16 +4,12 @@ var elBtnIngredient = document.getElementsByClassName('btnIngredient');
 var elSelIngredient = document.getElementsByClassName('selIngredient');
 
 for (let x = 0; x < elBtnIngredient.length; x++) {
-	elBtnIngredient[x].addEventListener('click', function(event) {
-		var ingredient = event.currentTarget.id.slice(6);
-		addIngredient(x, ingredient);
-	});
+	elBtnIngredient[x].addEventListener('click', (event) => addIngredient(x, event.currentTarget.id.slice(6)));
 }
 
 function addIngredient(x, ingredient) {
 	var selection = elSelIngredient[x].value;
 	var func = `add${ingredient}`;
-	console.log(selection, func);
 	SandwichMaker.addToTicket(SandwichMaker[func](selection));	
 	renderTicket(SandwichMaker.getTicket());
 	renderTotalPrice(SandwichMaker.getTotalPrice());	
@@ -21,9 +17,7 @@ function addIngredient(x, ingredient) {
 
 function renderTicket(ticket) {
 	var content = '';
-	for (var x = 0; x < ticket.length; x++) {
-		content += renderLine(ticket[x]);
-	}
+	ticket.forEach((ticket) => content += renderLine(ticket) );
 	ticketEl.innerHTML = content;
 }
 
@@ -35,45 +29,3 @@ function renderLine(line) {
 function renderTotalPrice(totalPrice) {
 	totalPriceEl.innerHTML = totalPrice;
 }
-
-// var selMeatEl = document.getElementById('meat');
-// var selBreadEl = document.getElementById('bread');
-// var selCheeseEl = document.getElementById('cheese');
-// var selVeggieEl = document.getElementById('veggie');
-// var selCondimentEl = document.getElementById('condiment');
-
-// function addMeat(x) {
-// 	console.log(elSelIngredient[x].value);
-// 	var selection = selMeatEl.value;
-// 	SandwichMaker.addToTicket(SandwichMaker.addMeat(selection));
-// 	renderTicket(SandwichMaker.getTicket());
-// 	renderTotalPrice(SandwichMaker.getTotalPrice());
-// }
-// function addBread(x) {
-// 	console.log(elSelIngredient[x].value);
-// 	var selection = selBreadEl.value;
-// 	SandwichMaker.addToTicket(SandwichMaker.addBread(selection));
-// 	renderTicket(SandwichMaker.getTicket());
-// 	renderTotalPrice(SandwichMaker.getTotalPrice());
-// }
-// function addCheese(x) {
-// 	console.log(elSelIngredient[x].value);
-// 	var selection = selCheeseEl.value;
-// 	SandwichMaker.addToTicket(SandwichMaker.addCheese(selection));
-// 	renderTicket(SandwichMaker.getTicket());
-// 	renderTotalPrice(SandwichMaker.getTotalPrice());
-// }
-// function addVeggie(x) {
-// 	console.log(elSelIngredient[x].value);
-// 	var selection = selVeggieEl.value;
-// 	SandwichMaker.addToTicket(SandwichMaker.addVeggie(selection));
-// 	renderTicket(SandwichMaker.getTicket());
-// 	renderTotalPrice(SandwichMaker.getTotalPrice());
-// }
-// function addCondiments(x) {
-// 	console.log(elSelIngredient[x].value);
-// 	var selection = selCondimentEl.value;
-// 	SandwichMaker.addToTicket(SandwichMaker.addCondiments(selection));
-// 	renderTicket(SandwichMaker.getTicket());
-// 	renderTotalPrice(SandwichMaker.getTotalPrice());
-// }
